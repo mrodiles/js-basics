f01789056ae8cc3e2d7ad770f8ba7696cd15c968
“Michael <michaelrodiles@gmail.com>
2022-01-09 13:07:25 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Exercise 1 Max of Two Numbers Simplest Form / Conditional Operators Form
+
+// const number = max(1, 2);
+// function max(a, b) {
+//   if (a > b) return a;
+//   return b;
+// }
+
+// Conditional operator ///
+
+//(condition) ? a : b
+
+const number = max(1, 2);
+function max(a, b) {
+  return a > b ? a : b;
+}
+console.log(number);
+
+
