b566f7581af3802d0e765aea218d2332cd6cd9ef
“Michael <michaelrodiles@gmail.com>
2022-01-12 22:05:30 -0500

    Even and Odd numbers Exercise 4

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-//Exercise 4 Demerit Points//
-//Speed Limit = 70
-// 5 over = 1 point
-//Math.floor(1.3)
-// driver gets more than 12 points license is suspended
+//Exercise 5 Even and Odd Numbers
 
-function checkSpeed(speed) {
-  const speedLimit = 70;
-  const kmPerPoint = 5;
-  if (speed < speedLimit + kmPerPoint) {
-    console.log("Ok");
-    return;
+showNumbers(10);
+
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
   }
-  const points = Math.floor((speed - speedLimit) / kmPerPoint);
-  if (points >= 12) console.log("License Suspended 👮‍♂️");
-  else console.log("Points", points);
 }
-
-checkSpeed(181);
