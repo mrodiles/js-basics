18d43ffc9e32b234b799286b0f73735f2d879d77
“Michael <michaelrodiles@gmail.com>
2022-01-22 22:55:41 -0500

    syntax

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //Exercise 8 Sum of Multiples of 3 and 5
-console.log(sum(10);
 
-// Multiples of 
-function sum(limit){
+// Multiples of
+function sum(limit) {
   const sum = 0;
 
-for (let i = 0; i <= limit; i++) 
-if (i % 3 === 0 || i % 5 === 0)
-sum += i;
+  for (let i = 0; i <= limit; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
 
-return sum;
+  return sum;
 }
+
+console.log(sum(10));
