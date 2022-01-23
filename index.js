ffaa1179188411f1f4448a189c10cc473c78dfdd
“Michael <michaelrodiles@gmail.com>
2022-01-22 23:15:04 -0500

    Grades exercise 9 cleaner code

--- index.js
+++ index.js
@@ -11,15 +11,19 @@ const marks = [80, 80, 50];
 console.log(calculate(marks));
 
 function calculate(marks) {
-  const sun = 0;
-  for (let mark of marks) {
-    sum += mark;
-    let average = sum / marks.length;
+  const average = calculateAverage(marks);
 
-    if (average < 60) return "F";
-    if (average < 70) return "D";
-    if (average < 80) return "C";
-    if (average < 90) return "B";
-    return "A";
+  if (average < 60) return "F";
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return "A";
+}
+
+function calculateAverage(array) {
+  const sun = 0;
+  for (let value of array) {
+    sum += value;
+    return sum / array.length;
   }
 }
