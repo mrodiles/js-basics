40df6eac49ce60bb8368d59169790c0b61330bdb
“Michael <michaelrodiles@gmail.com>
2022-01-22 23:08:47 -0500

    Grades Exercise 9

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-//Exercise 8 Sum of Multiples of 3 and 5
+const marks = [80, 80, 50];
 
-// Multiples of
-function sum(limit) {
-  const sum = 0;
+//Average = 70
 
-  for (let i = 0; i <= limit; i++) if (i % 3 === 0 || i % 5 === 0) sum += i;
+//1-59: F
+// 60-69: D
+// 70- 79: C
+// 80-89: B
+// 90-100: A
 
-  return sum;
+console.log(calculate(marks));
+
+function calculate(marks) {
+  const sun = 0;
+  for (let mark of marks) {
+    sum += mark;
+    let average = sum / marks.length;
+
+    if (average < 60) return "F";
+    if (average < 70) return "D";
+    if (average < 80) return "C";
+    if (average < 90) return "B";
+    return "A";
+  }
 }
-
-console.log(sum(10));
